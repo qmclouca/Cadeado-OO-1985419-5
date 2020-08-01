04b8315628ded33a6d170664d553f758f66cf122
Rodolfo Bortoluzzi <qmclouca@gmail.com>
2020-08-01 16:58:47 -0300

    Revert "inserindo código de comparação, falta método de alteração de senha"
    This reverts commit 5372a5e45c90887214398acd00370b8cc11f5dca.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,8 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.Segredo = new System.Windows.Forms.GroupBox();
             this.checkBox5 = new System.Windows.Forms.CheckBox();
             this.checkBox3 = new System.Windows.Forms.CheckBox();
@@ -52,7 +50,6 @@
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.Segredo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.digito5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.digito4)).BeginInit();
@@ -76,178 +73,273 @@
             this.Segredo.Controls.Add(this.digito2);
             this.Segredo.Controls.Add(this.label1);
             this.Segredo.Controls.Add(this.digito1);
-            resources.ApplyResources(this.Segredo, "Segredo");
+            this.Segredo.Location = new System.Drawing.Point(12, 12);
             this.Segredo.Name = "Segredo";
+            this.Segredo.Size = new System.Drawing.Size(200, 279);
+            this.Segredo.TabIndex = 2;
             this.Segredo.TabStop = false;
+            this.Segredo.Text = "Segredo";
             this.Segredo.Enter += new System.EventHandler(this.Segredo_Enter);
             // 
             // checkBox5
             // 
-            resources.ApplyResources(this.checkBox5, "checkBox5");
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Enabled = false;
+            this.checkBox5.Location = new System.Drawing.Point(157, 242);
             this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(40, 17);
+            this.checkBox5.TabIndex = 12;
+            this.checkBox5.Text = "Ok";
             this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.Visible = false;
             this.checkBox5.CheckStateChanged += new System.EventHandler(this.checkBox5_CheckStateChanged);
             // 
             // checkBox3
             // 
-            resources.ApplyResources(this.checkBox3, "checkBox3");
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Enabled = false;
+            this.checkBox3.Location = new System.Drawing.Point(157, 140);
             this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(40, 17);
+            this.checkBox3.TabIndex = 10;
+            this.checkBox3.Text = "Ok";
             this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.Visible = false;
             this.checkBox3.CheckStateChanged += new System.EventHandler(this.checkBox3_CheckStateChanged);
             // 
             // checkBox1
             // 
-            resources.ApplyResources(this.checkBox1, "checkBox1");
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(157, 37);
             this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(40, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Ok";
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckStateChanged += new System.EventHandler(this.checkBox1_CheckStateChanged);
             // 
             // label5
             // 
-            resources.ApplyResources(this.label5, "label5");
+            this.label5.AutoSize = true;
             this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label5.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label5.Location = new System.Drawing.Point(157, 224);
             this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "5";
+            this.label5.Visible = false;
             this.label5.Click += new System.EventHandler(this.digito5_ValueChanged);
             // 
             // digito5
             // 
             this.digito5.Cursor = System.Windows.Forms.Cursors.Hand;
-            resources.ApplyResources(this.digito5, "digito5");
+            this.digito5.Location = new System.Drawing.Point(7, 224);
             this.digito5.Maximum = 9;
             this.digito5.Name = "digito5";
+            this.digito5.Size = new System.Drawing.Size(144, 45);
+            this.digito5.TabIndex = 4;
             this.digito5.Value = 5;
+            this.digito5.Visible = false;
             this.digito5.Scroll += new System.EventHandler(this.digito5_ValueChanged);
             this.digito5.ValueChanged += new System.EventHandler(this.digito5_ValueChanged_1);
             // 
             // label4
             // 
-            resources.ApplyResources(this.label4, "label4");
+            this.label4.AutoSize = true;
             this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(157, 173);
             this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "5";
+            this.label4.Visible = false;
             this.label4.Click += new System.EventHandler(this.digito4_ValueChanged);
             // 
             // digito4
             // 
             this.digito4.Cursor = System.Windows.Forms.Cursors.Hand;
-            resources.ApplyResources(this.digito4, "digito4");
+            this.digito4.Location = new System.Drawing.Point(6, 173);
             this.digito4.Maximum = 9;
             this.digito4.Name = "digito4";
+            this.digito4.Size = new System.Drawing.Size(145, 45);
+            this.digito4.TabIndex = 3;
             this.digito4.Value = 5;
+            this.digito4.Visible = false;
             this.digito4.Scroll += new System.EventHandler(this.digito4_ValueChanged);
             this.digito4.ValueChanged += new System.EventHandler(this.digito4_ValueChanged_1);
             // 
             // label3
             // 
-            resources.ApplyResources(this.label3, "label3");
+            this.label3.AutoSize = true;
             this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(157, 122);
             this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "5";
+            this.label3.Visible = false;
             this.label3.Click += new System.EventHandler(this.digito3_ValueChanged);
             // 
             // digito3
             // 
             this.digito3.Cursor = System.Windows.Forms.Cursors.Hand;
-            resources.ApplyResources(this.digito3, "digito3");
+            this.digito3.Location = new System.Drawing.Point(6, 122);
             this.digito3.Maximum = 9;
             this.digito3.Name = "digito3";
+            this.digito3.Size = new System.Drawing.Size(145, 45);
+            this.digito3.TabIndex = 2;
             this.digito3.Value = 5;
+            this.digito3.Visible = false;
             this.digito3.Scroll += new System.EventHandler(this.digito3_ValueChanged);
             this.digito3.ValueChanged += new System.EventHandler(this.digito3_ValueChanged_1);
             // 
             // label2
             // 
-            resources.ApplyResources(this.label2, "label2");
+            this.label2.AutoSize = true;
             this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(157, 71);
             this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "5";
+            this.label2.Visible = false;
             this.label2.Click += new System.EventHandler(this.digito2_ValueChanged);
             // 
             // digito2
             // 
             this.digito2.Cursor = System.Windows.Forms.Cursors.Hand;
-            resources.ApplyResources(this.digito2, "digito2");
+            this.digito2.Location = new System.Drawing.Point(6, 71);
             this.digito2.Maximum = 9;
             this.digito2.Name = "digito2";
+            this.digito2.Size = new System.Drawing.Size(145, 45);
+            this.digito2.TabIndex = 1;
             this.digito2.Value = 5;
+            this.digito2.Visible = false;
             this.digito2.Scroll += new System.EventHandler(this.digito2_ValueChanged);
             this.digito2.ValueChanged += new System.EventHandler(this.digito2_ValueChanged_1);
             // 
             // label1
             // 
-            resources.ApplyResources(this.label1, "label1");
+            this.label1.AutoSize = true;
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Location = new System.Drawing.Point(157, 19);
             this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "5";
             // 
             // digito1
             // 
             this.digito1.Cursor = System.Windows.Forms.Cursors.Hand;
-            resources.ApplyResources(this.digito1, "digito1");
+            this.digito1.Location = new System.Drawing.Point(7, 19);
             this.digito1.Maximum = 9;
             this.digito1.Name = "digito1";
+            this.digito1.Size = new System.Drawing.Size(144, 45);
+            this.digito1.TabIndex = 0;
             this.digito1.Value = 5;
             this.digito1.Scroll += new System.EventHandler(this.digito1_ValueChanged);
             this.digito1.ValueChanged += new System.EventHandler(this.digito1_ValueChanged_1);
             // 
             // checkBox2
             // 
-            resources.ApplyResources(this.checkBox2, "checkBox2");
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Enabled = false;
+            this.checkBox2.Location = new System.Drawing.Point(169, 101);
             this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(40, 17);
+            this.checkBox2.TabIndex = 9;
+            this.checkBox2.Text = "Ok";
             this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.Visible = false;
             this.checkBox2.CheckStateChanged += new System.EventHandler(this.checkBox2_CheckStateChanged);
             // 
             // checkBox4
             // 
-            resources.ApplyResources(this.checkBox4, "checkBox4");
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Enabled = false;
+            this.checkBox4.Location = new System.Drawing.Point(169, 203);
             this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(40, 17);
+            this.checkBox4.TabIndex = 11;
+            this.checkBox4.Text = "Ok";
             this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.Visible = false;
             this.checkBox4.CheckStateChanged += new System.EventHandler(this.checkBox4_CheckStateChanged);
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            resources.ApplyResources(this.button1, "button1");
+            this.button1.Enabled = false;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Rockwell Extra Bold", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(12, 351);
             this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 41);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "ABRIR CADEADO";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
-            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
-            this.button1.Validated += new System.EventHandler(this.checkBox5_CheckStateChanged);
             // 
             // label6
             // 
-            resources.ApplyResources(this.label6, "label6");
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(9, 312);
             this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 25);
+            this.label6.TabIndex = 13;
+            this.label6.Visible = false;
             // 
             // label7
             // 
-            resources.ApplyResources(this.label7, "label7");
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(50, 312);
             this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 25);
+            this.label7.TabIndex = 14;
+            this.label7.Visible = false;
             // 
             // label8
             // 
-            resources.ApplyResources(this.label8, "label8");
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(91, 312);
             this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 25);
+            this.label8.TabIndex = 15;
+            this.label8.Visible = false;
             // 
             // label9
             // 
-            resources.ApplyResources(this.label9, "label9");
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(132, 312);
             this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 25);
+            this.label9.TabIndex = 16;
+            this.label9.Visible = false;
             // 
             // label10
             // 
-            resources.ApplyResources(this.label10, "label10");
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(173, 312);
             this.label10.Name = "label10";
-            // 
-            // timer1
-            // 
-            this.timer1.Enabled = true;
+            this.label10.Size = new System.Drawing.Size(0, 25);
+            this.label10.TabIndex = 17;
+            this.label10.Visible = false;
             // 
             // Form1
             // 
-            resources.ApplyResources(this, "$this");
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 404);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
@@ -257,11 +349,8 @@
             this.Controls.Add(this.checkBox4);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.Segredo);
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "Form1";
-            this.ShowIcon = false;
-            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Cadeado OO";
             this.Segredo.ResumeLayout(false);
             this.Segredo.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.digito5)).EndInit();
@@ -297,7 +386,6 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-
 namespace Cadeado_OO_1985419_5
 {
     public partial class Form1 : Form
@@ -18,10 +17,7 @@ namespace Cadeado_OO_1985419_5
             InitializeComponent();
         }
 
-        int senha = 58419;
-
-        public int Senha { get => senha; set => senha = value; }
-
+        
         private void AddMyScrollEventHandlers()
         {
             // Create and initialize a VScrollBar.
@@ -35,109 +31,139 @@ namespace Cadeado_OO_1985419_5
                this.digito1_Scroll);
             digito1.ValueChanged += new EventHandler(
                this.digito1_ValueChanged);
+
             digito2.Scroll += new ScrollEventHandler(
                   this.digito2_Scroll);
             digito2.ValueChanged += new EventHandler(
                this.digito2_ValueChanged);
+
             digito3.Scroll += new ScrollEventHandler(
               this.digito3_Scroll);
             digito3.ValueChanged += new EventHandler(
                this.digito3_ValueChanged);
+
             digito4.Scroll += new ScrollEventHandler(
               this.digito4_Scroll);
             digito4.ValueChanged += new EventHandler(
                this.digito4_ValueChanged);
+
             digito5.Scroll += new ScrollEventHandler(
               this.digito5_Scroll);
             digito5.ValueChanged += new EventHandler(
                this.digito5_ValueChanged);
+
+
         }
+
+        // Create the ValueChanged event handler.
         private void digito1_ValueChanged(Object sender,
                                               EventArgs e)
         {
             // Display the new value in the label.
             label1.Text = "vScrollBar Value:(OnValueChanged Event) " + digito1.Value.ToString();
         }
+
         private void digito2_ValueChanged(Object sender,
                                              EventArgs e)
         {
             // Display the new value in the label.
             label2.Text = "vScrollBar Value:(OnValueChanged Event) " + digito2.Value.ToString();
         }
+
         private void digito3_ValueChanged(Object sender,
                                              EventArgs e)
         {
             // Display the new value in the label.
             label3.Text = "vScrollBar Value:(OnValueChanged Event) " + digito3.Value.ToString();
         }
+
         private void digito4_ValueChanged(Object sender,
                                              EventArgs e)
         {
             // Display the new value in the label.
             label4.Text = "vScrollBar Value:(OnValueChanged Event) " + digito4.Value.ToString();
         }
+
         private void digito5_ValueChanged(Object sender,
                                              EventArgs e)
         {
             // Display the new value in the label.
             label5.Text = "vScrollBar Value:(OnValueChanged Event) " + digito5.Value.ToString();
         }
+
+        // Create the Scroll event handler.
         private void digito1_Scroll(Object sender,
                                         ScrollEventArgs e)
         {
             // Display the new value in the label.
             label1.Text = "VScrollBar Value:(OnScroll Event) " + e.NewValue.ToString();
         }
+
         private void digito2_Scroll(Object sender,
                                        ScrollEventArgs e)
         {
             // Display the new value in the label.
             label2.Text = "VScrollBar Value:(OnScroll Event) " + e.NewValue.ToString();
         }
+
         private void digito3_Scroll(Object sender,
                                        ScrollEventArgs e)
         {
             // Display the new value in the label.
             label3.Text = "VScrollBar Value:(OnScroll Event) " + e.NewValue.ToString();
         }
+
         private void digito4_Scroll(Object sender,
                                        ScrollEventArgs e)
         {
             // Display the new value in the label.
             label4.Text = "VScrollBar Value:(OnScroll Event) " + e.NewValue.ToString();
         }
+
         private void digito5_Scroll(Object sender,
                                        ScrollEventArgs e)
         {
             // Display the new value in the label.
             label5.Text = "VScrollBar Value:(OnScroll Event) " + e.NewValue.ToString();
         }
+
         private void Button1_Click(Object sender,
                                   EventArgs e)
         {
-
+           
 
         }
+
         private void digito1_ValueChanged_1(object sender, EventArgs e)
         {
             label1.Text = digito1.Value.ToString();
+
         }
+
         private void digito2_ValueChanged_1(object sender, EventArgs e)
         {
             label2.Text = digito2.Value.ToString();
+
         }
+
         private void digito3_ValueChanged_1(object sender, EventArgs e)
         {
             label3.Text = digito3.Value.ToString();
+
         }
+
         private void digito4_ValueChanged_1(object sender, EventArgs e)
         {
             label4.Text = digito4.Value.ToString();
+
         }
+
         private void digito5_ValueChanged_1(object sender, EventArgs e)
         {
             label5.Text = digito5.Value.ToString();
+
         }
+
         private void checkBox1_CheckStateChanged(object sender, EventArgs e)
         {
             digito1.Enabled = false;
@@ -151,6 +177,7 @@ namespace Cadeado_OO_1985419_5
 
 
         }
+
         private void checkBox2_CheckStateChanged(object sender, EventArgs e)
         {
             digito2.Enabled = false;
@@ -162,6 +189,7 @@ namespace Cadeado_OO_1985419_5
             label7.Text = label2.Text;
             label7.Visible = true;
         }
+
         private void checkBox3_CheckStateChanged(object sender, EventArgs e)
         {
             digito3.Enabled = false;
@@ -174,6 +202,7 @@ namespace Cadeado_OO_1985419_5
             label8.Visible = true;
 
         }
+
         private void checkBox4_CheckStateChanged(object sender, EventArgs e)
         {
             digito4.Enabled = false;
@@ -185,6 +214,7 @@ namespace Cadeado_OO_1985419_5
             label9.Text = label4.Text;
             label9.Visible = true;
         }
+
         private void checkBox5_CheckStateChanged(object sender, EventArgs e)
         {
             digito5.Enabled = false;
@@ -200,27 +230,7 @@ namespace Cadeado_OO_1985419_5
         {
 
         }
-        private void button1_MouseClick(object sender, MouseEventArgs e)
-        {
-            int senhainformada = digito1.Value * 10000 + digito2.Value * 1000 + digito3.Value * 100 + digito4.Value * 10 + digito5.Value;
-            timer1.Enabled = true;
-            if (senha.Equals(senhainformada))
-            {
-                button1.Text = "Senha Correta!";
-                button1.BackColor = Color.BlueViolet;
-            
-            } else
-            {
-                button1.BackColor = Color.Red;
-                button1.Text = "Senha Incorreta";
-                if (timer1.Equals(10000))
-                {
-                    Application.Exit();
-                }
 
-            }
-        }
-        
-    
+       
     }
 }
